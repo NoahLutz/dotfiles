243020654e29fcac1e9533bf348a4045951aa50a
Noah Lutz <noah.l98@gmail.com>
2019-11-15 09:11:11 -0500

    added REPO_OWNER var to change repository owner

--- bin/tmuxProject.sh
+++ bin/tmuxProject.sh
@@ -2,6 +2,7 @@
 
 
 TIMEOUT=60
+REPO_OWNER=NoahLutz
 
 # Checks if tmux session with name of $1 exits
 # Arguments
@@ -85,7 +86,7 @@ create_session()
       # Check if repo needs to be cloned
       if $cloneRepoFlag; then
          local elapsedTime=0
-         local repoURL=git@github.com:VoltServer/$sessionName.git
+         local repoURL=git@github.com:$REPO_OWNER/$sessionName.git
          tmux -S $socket send-keys -t "$sessionName:terminal" "cd ~/src/" C-m
          sleep 1 # sleep to let command execute
          tmux -S $socket send-keys -t "$sessionName:terminal" -l "git clone $repoURL"
